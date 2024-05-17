36e29f60fd247d58c083174fcaaff2c736c37f30
leeoos <leonardo.colosi@gmail.com>
2024-05-17 16:32:09 +0200

    game mechanics

--- server/pepper.py
+++ server/pepper.py
@@ -2,25 +2,8 @@ import os
 import re
 import time
 import json
-# import fcntl
 import random
 import threading
-
-def check_key(stop_event):
-    input("Press Enter to exit the loop.\n")
-    stop_event.set()
-
-# stop_event = threading.Event()
-# key_thread = threading.Thread(target=check_key, args=(stop_event,))
-# key_thread.start()
-
-semaphore_file = 'semaphore.txt'
-
-def set_semaphore(status):
-    with open(semaphore_file, 'w') as file:
-        # fcntl.flock(file, fcntl.LOCK_EX)  # Acquire exclusive lock
-        file.write(status)
-        # fcntl.flock(file, fcntl.LOCK_UN)  # Release lock
 
 def get_progressive_id(filename):
   with open(filename, 'r') as file:
@@ -37,7 +20,7 @@ import json
 def update_progressive_id(filename, new_id):
   with open(filename, 'r+') as file:
     # Read the first part of the file where "progressive_id" is located
-    content = file.read(100)  # Adjust this value if necessary
+    content = file.read(100) 
     
     # Locate the start and end of the "progressive_id" value
     start = content.find('"progressive_id"') + len('"progressive_id": ')
@@ -88,17 +71,19 @@ def add_players(players, current_roles, max_players, enable_test=False):
 
   while num_players <= max_players:
 
-    playes_info = {}
+    player_info = {}
 
     if new_player:
       num_players += 1
-      qr_code = "player_code " + str(num_players)
+      qr_code = "player_code_" + str(num_players)
       add_role(current_roles)
-      playes_info["player_id"] = qr_code
-      playes_info["status"] = "alive"
-      players.append(playes_info)
+      player_info['player_id'] = qr_code
+      player_info['status'] = "alive"
+      player_info['votes'] = 0
+      
+      players.append(player_info)
 
-    # change this to work with pepper chat
+    # TODO: change this to work with pepper chat
     if num_players < max_players:
       if enable_test: 
         new_player = True
@@ -121,27 +106,42 @@ def add_players(players, current_roles, max_players, enable_test=False):
        pass
 
 
-def assign_roles(game_dict, players, current_roles):
+def assign_roles(
+    game_info, 
+    players, 
+    current_roles, 
+    alive_wolves, 
+    alive_no_wolves
+):
   random.shuffle(players)
   random.shuffle(current_roles)
-  print(players)
-  print(current_roles)
+
   for player, role in zip(players, current_roles):
-    player["role"] = role
-    game_dict["players"].append(player)
+    player['role'] = role
+    game_info['players'].append(player)
+
+    if role == "wolf": alive_wolves += 1
+    else: alive_no_wolves += 1
+
+
+def save_cache(cache_filename, game_info):
+  with open(cache_filename, "w") as game_file:
+    json.dump(game_info, game_file, indent=4)
 
 
 def initialize_game(max_players=8, enable_test=False):
 
-  # parametrs variables
+  # game parametrs
+  database_filename = "database.json"
   players = []
   current_roles = []
+  alive_wolves = 0
+  alive_no_wolves = 0
 
-  # ask the human if start a new game or continue an old one
+  # TODO: ask the human if start a new game or continue an old one
   new_game = True
 
   if new_game:
-    database_filename = "database.json"
 
     # set game id
     progressive_id = get_progressive_id(database_filename)
@@ -152,46 +152,120 @@ def initialize_game(max_players=8, enable_test=False):
     cache_filename = f"game_{new_progressive_id}_cache.json"
 
     # initialize cache database for new game
-    game_dict = {
+    game_info = {
         "game_id": new_progressive_id,
         "status": "active",
         "round": 1,
         "night": True,
         "players": []   
     }
-
   
+  else:
+    # TODO: load old game
+    ...
+
   add_players(players, current_roles, max_players)
-  
-  assign_roles(game_dict, players, current_roles)
+  assign_roles(
+    game_info, 
+    players, 
+    current_roles, 
+    alive_wolves, 
+    alive_no_wolves
+  ) 
+  save_cache(cache_filename, game_info) # initialize new game
 
-  # initialize new game file to share info with server 
-  with open(cache_filename, "w") as game_file:
-    json.dump(game_dict, game_file, indent=4)
+  while True:
 
+    print(f"alive villagers: {alive_no_wolves}")
+    print(f"alive wolves: {alive_wolves}")
   
-  ### Here implement the game logic ###
+    # TODO: implement here the game logic 
+    # TODO: wait for the response from the web
+    # TODO: get id of player_to kill
+
+    player_to_kill = "player_code_2" # to get from the web
+
+    with open(cache_filename, "r") as game_file:
+      game_info = json.load(game_file)
+
+    # update the state of all the players during night
+    if game_info['night']:
+      for player in game_info['players']:
+        is_alive = (player['status'] == "alive")
+        if is_alive and player['player_id'] == player_to_kill:
+          if player['role'] == "wolf":
+            # TODO: send this message to the client 
+            raise  Exception("Error: wolves cannot kill each others")
+          print(f"killing: {player['player_id']}")
+          player['status'] = "dead"
+          alive_no_wolves -= 1
+          break
+
+    # update the state of the game
+    game_info['night'] = False
+    save_cache(cache_filename, game_info)
+    
+    # TODO: get a list of votes for each charater form the web page
+    foo = {"player_code_1": 5}
+
+    for player in game_info['players']:
+      if player['player_id'] in foo.keys():
+        player['votes'] = foo[player['player_id']]
+
+    # update the state of all the players during day
+    if not game_info['night']:
+      # TODO: filter the players that are alive with the majority of votes
+      candidates = filter(
+        lambda x: (
+          x['votes'] == max(
+            game_info['players'], key=lambda y: y['votes']
+          )['votes']
+          and 
+          x['status'] == "alive"
+        ),
+        game_info['players']
+      )
+
+      candidates = list(candidates)
+      if len(candidates) == 1:
+        candidate_id = game_info['players'].index(candidates[0])
+      
+      else:
+        # TODO: here notify the client to do anoter voting
+        ...
+
+      game_info['players'][candidate_id]['status'] = "dead"
+      if game_info['players'][candidate_id]['role'] == "wolf":
+        alive_wolves -= 1
+      else:
+        alive_no_wolves -= 1
+    
+
+    # update the state of the game
+    save_cache(cache_filename, game_info)
+
+    # TODO: check for stop condictions
+    if alive_wolves <= 0:
+      # TODO: make pepper say this
+      print("villagers win")
+      break
+
+    game_info['round'] += 1
+    break
+
+
+    # end of loop
+
+  # termination
+  game_info['status'] = "inactive"
+  save_cache(cache_filename, game_info)
 
   # update database with final game 
   update_progressive_id(database_filename, new_progressive_id)
 
 
+
+
 if __name__ == "__main__":
 
   initialize_game()
-
-  # test()
-
-  # # Example: Set the semaphore to green after 10 seconds
-  # while not stop_event.is_set():
-  #   set_semaphore('green')
-  #   print('Semaphore set to green')
-  #   time.sleep(10)
-
-  #   set_semaphore('red')
-  #   print('Semaphore set to red')
-  #   time.sleep(10)
-
-
-    
-  # set_semaphore('red')
