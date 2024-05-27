0bd82151d1a128edc5407a68e2260907a9155ac2
BrFranceN <workbrfrancen@gmail.com>
2024-05-27 14:25:14 +0200

    list of player generated correctly

--- clients/client.js
+++ clients/client.js
@@ -3,7 +3,7 @@ async function fetchData() {
     try {
         const response = await fetch('http://127.0.0.1:5000/request_data');
         const data = await response.json();
-        console.log(data);  // Logging the entire dictionary
+        // console.log(data);  // Logging the entire dictionary
         return data;
     } catch (error) {
         console.error('Error fetching game info:', error);
---
+++ utils/utils.py
@@ -0,0 +1,15 @@
+import osimport sys
+import json
+
+root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(root_dir)
+
+def load_data_from_json(filename):
+  data = None
+  with open(filename, 'r') as f:
+    data = json.load(f)
+  return data
+
+def save_data_to_json(filename, data):
+  with open(filename, "w") as f:
+    json.dump(data, f, indent=4)
