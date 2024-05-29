61db0ebd6d4a455c52350a0e21bbbf4273d90774
leeoos <leoanrdo.colosi@gmail.com>
2024-05-29 19:55:58 +0200

    update imports

--- utils/utils.py
+++ utils/utils.py
@@ -2,9 +2,6 @@ import os
 import sys
 import json
 
-root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(root_dir)
-
 def load_data_from_json(filename):
   data = None
   with open(filename, 'r') as f:
