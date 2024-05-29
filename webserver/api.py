3b2b63e9d1e32cd29722b94f7d35eae53dd618de
leeoos <leoanrdo.colosi@gmail.com>
2024-05-29 23:42:47 +0200

    update: now it is possible to use the web page from another machine on the same network

--- webserver/api.py
+++ webserver/api.py
@@ -1,4 +1,5 @@
 import json
+import time
 import socket
 from copy import deepcopy
 from threading import Thread
@@ -12,52 +13,54 @@ class API():
     self.port = port
     self.game_info = game_info
     self.server_socket = self.make_socket(host, port)
-    
-        
+    self.conn, self.addr = None, None
     
   def make_socket(self, host, port):
       server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       server_socket.bind((host, port))
       server_socket.listen(1)
       print(f"New socket --> ip: {self.host}, port: {self.port}")
-      return server_socket      
+      return server_socket   
+
+  def start_connection(self):
+      time.sleep(5)
+      print(f"Waiting for a connection... on port {self.port}")
+      self.conn, self.addr = self.server_socket.accept() 
+    #   return self.server_socket.accept() 
 
   def listen(self):
-      print(f"Waiting for a connection... on port {self.port}")
-      conn, addr = self.server_socket.accept()
+    #   conn, addr = self.server_socket.accept()
+      while not self.conn:
+          pass
+      
+      print(f"Waiting for docker bridge...")
 
-      with conn:
-          print('Connected by', addr)
+      with self.conn:
+          print('Connected by', self.addr)
           while True:
-              data = conn.recv(1024)
+              data = self.conn.recv(1024)
               if not data:
                   break
               print(f"\nReceived data: {data.decode('utf-8')} \n")
               new_data = json.loads(data.decode('utf-8'))
               self.update_game_info(new_data)
-              conn.sendall("Data received".encode('utf-8'))
-              # self.game_info = deepcopy(data.decode('utf-8'))
-              # conn.sendall("Data received".encode('utf-8'))
-              # self.game_info = json.loads(self.game_info)
+              self.conn.sendall("Data received".encode('utf-8'))
       
   def update_game_info(self, new_data):
       self.game_info.clear()
       self.game_info.update(new_data)
 
   def send_back(self, player_vote):
-      conn, addr = self.server_socket.accept()
-      print(f"Sending data to bridge {self.port}")
+    #   conn, addr = self.server_socket.accept()
+    print(f"Sending data to bridge on port:  {self.port}")
+    send_data = json.dumps(player_vote)
+    self.conn.sendall(send_data.encode('utf-8'))
 
-      with conn:
-        # print("Sending data to bridge")
-        send_data = json.dumps(player_vote)
-        conn.sendall(send_data.encode('utf-8'))
-        # reset player vote dictionary
-        # num_votes = 0 
-        # player_vote = {}
-      return True
+    #   return True
   
-  def start_linking(self):
-     socket_thread = Thread(target=self.listen, args=())
-     socket_thread.start()
-     print("end linking")
+  def start_listening(self):
+        connection_thread = Thread(target=self.start_connection, args=())
+        connection_thread.start()
+        socket_thread = Thread(target=self.listen, args=())
+        socket_thread.start()
+        print("all thread running")
